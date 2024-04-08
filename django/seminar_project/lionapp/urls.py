ffbddfe48113ca30448dc057f85823d039d06ad5
lovehachiware <dante3518@naver.com>
2024-04-08 19:30:56 +0900

    장고심화세션

---
+++ django/seminar_project/lionapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('create/',views.create_post),
+    path('<int:pk>/',views.get_post),
+    path('delete/<int:pk>',views.delete_post)
+]
