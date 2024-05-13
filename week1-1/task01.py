c5aeedbb8ba687b636e3c5846184d877a51437cf
lovehachiware <dante3518@naver.com>
2024-05-13 20:48:20 +0900

    re commit

--- week1-1/task01.py
+++
@@ -1,5 +0,0 @@
-a = "Hello"
-b = " "
-c = "World"
-
-print(a+b+c+b+a+b+c)
--- week1-1/task03.py
+++
@@ -1,3 +0,0 @@
-itsme = ["백엔드","심성민","infj","26"]
-
-print("안녕하세요 저는 멋쟁이 사자처럼 %s파트이고\n이름은 %s입니다\n제 mbti는 %s이고\n나이는 %s입니다." % (itsme[0],itsme[1],itsme[2],itsme[3]))
