bdf52958a7095741301d274cfb11cf5be99ffe08
lovehachiware <dante3518@naver.com>
2024-05-13 20:52:07 +0900

    re commit

--- django/django_projects/lionapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post,Comment
-# Register your models here.
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)  # 사이트에서 admin으로 얘네를 관리하겠다
